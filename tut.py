3497584b8b7d62ac4f7659f3447e0c7c9488e224
Neal Pathak <nealpathak94@gmail.com>
2019-06-25 13:18:04 -0500

    Update tut.py

--- tut.py
+++ tut.py
@@ -4,14 +4,19 @@ game = [[0, 0, 0],
 
 
 def game_board(game_map, player=0, row=0, column=0, just_display=False):
-    print("   0  1  2")
-    if not just_display:
-        game_map[row][column] = player
-    for count, row in enumerate(game_map):
-        print(count, row)
+    try:
+        print("   0  1  2")
+        if not just_display:
+            game_map[row][column] = player
+        for count, row in enumerate(game_map):
+            print(count, row)
+        return game_map
 
-    return game_map
+    except IndexError as e:
+        print("Error: Make sure you input row/column as 0 1 or 2?", e)
 
+    except Exception as e:
+        print("Something went very wrong!", e)
 
 game = game_board(game, just_display=True)
-game = game_board(game, player=1, row=2, column=1)
+game = game_board(game_board, player=1, row=3, column=1)
