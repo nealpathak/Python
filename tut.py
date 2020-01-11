d2b7b0aa474c12729ea1f3cb19d349b44b95edbd
Neal Pathak <nealpathak94@gmail.com>
2020-01-11 03:37:52 -0600

    stuff

--- tut.py
+++ tut.py
@@ -1,37 +1,51 @@
 game = [[1, 0, 2],
-        [1, 1, 1],
-        [2, 2, 1],]
+        [2, 2, 2],
+        [1, 2, 1],]
 
-diags = []
-for ix in range(len(game)):
-    diags.append(game[ix][ix])
-print(diags)
-
-if game[2][0] == game[1][1] == game[0][2]:
-    print("Winner")
-
-
-
-
-
-
-'''
-for col in range(len(game)):
-    check = []
-
-    for row in game:
-        check.append(row[0])
-
-    if check.count(check[0]) == len(check) and check[0] != 0:
-        print("Winner!")
-'''
-'''
 def win(current_game):
+    #Horizontal
     for row in game:
         print(row)
         if row.count(row[0]) == len(row) and row[0] != 0:
-            print("Winner!")
+            print(f"Player {row[0]} is the winner horizontally")
+            
+    #Diagonal
+    diags = []
+    for col, row in enumerate(reversed(range(len(game)))):
+        diags.append(game[row][col])
+    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+            print(f"Player {diags[0]} is the winner diagonally (/)")
 
+    diags = []
+    for ix in range(len(game)):
+        diags.append(game[ix][ix])
+    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+            print(f"Player {diags[0]} is the winner diagonally (\\)")
 
-win(game)
-'''
+    #Vertical
+    for col in range(len(game)):
+        check = []
+
+        for row in game:
+            check.append(row[col])
+
+        if check.count(check[0]) == len(check) and check[0] != 0:
+            print(f"Player {check[0]} is the winner vertically")
+
+def game_board(game_map, player=0, row=0, column=0, just_display=False):
+    try:
+        print("   0  1  2")
+        if not just_display:
+            game_map[row][column] = player
+        for count, row in enumerate(game_map):
+            print(count, row)
+        return game_map
+
+    except IndexError as e:
+        print("Error: make sure you input row/column as 0 1 or 2?", e)
+
+    except Exception as e:
+        print("Something went very wrong!", e)
+
+game = game_board(game, just_display=True)
+game = game_board(game_board, player=1, row=3, column=1)
